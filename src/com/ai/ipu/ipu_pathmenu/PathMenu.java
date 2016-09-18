523ac7b56bdb12146cf0a6cf572ad0fcc166d802
linh <836340401@qq.com>
2016-09-18 15:47:05 +0800

    更新：悬浮+拖动+更改子菜单方向。还未写service

--- src/com/ai/ipu/ipu_pathmenu/PathMenu.java
+++ src/com/ai/ipu/ipu_pathmenu/PathMenu.java
@@ -2,11 +2,15 @@ package com.ai.ipu.ipu_pathmenu;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.PixelFormat;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
@@ -18,6 +22,7 @@ import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /**
  * 自定义菜单
@@ -25,18 +30,50 @@ import android.widget.RelativeLayout;
  * @author 何凌波
  *
  */
-public class PathMenu extends RelativeLayout {
+public class PathMenu extends RelativeLayout implements OnTouchListener {
 	private PathMenuLayout mPathMenuLayout;
 	private ImageView mHintView;// 中心按钮显示图片
+	private WindowManager mWindowManager;// 当前view的窗口管理器
+	// WindowMananger的params，控制这个值可以将自定义的view设置到窗口管理器中
+	private WindowManager.LayoutParams mWmParams;
+	private float mTouchStartX;// 记录首次按下的位置x
+	private float mTouchStartY;// 记录首次按下的位置y
+
+	private int mScreenWidth;// 屏幕宽度
+	private int mScreenHeight;// 屏幕高度
+	private boolean mDraging;// 是否拖动中
+
+	private Context mContext;
+
+	private int btnWidth;// 按钮宽度
+	private int btnHeight;// 按钮高度
+
+	private int position;// 按钮的位置
+	private int LEFT_TOP = 1;
+	private int CENTER_TOP = 2;
+	private int RIGHT_TOP = 3;
+	private int LEFT_CENTER = 4;
+	private int CENTER = 5;
+	private int RIGHT_CENTER = 6;
+	private int LEFT_BOTTOM = 7;
+	private int CENTER_BOTTOM = 8;
+	private int RIGHT_BOTTOM = 9;
+
+	private static final int[] ITEM_DRAWABLES = { R.drawable.composer_camera,
+			R.drawable.composer_music, R.drawable.composer_place,
+			R.drawable.composer_sleep, R.drawable.composer_thought,
+			R.drawable.composer_with };
 
 	public PathMenu(Context context) {
 		super(context);
-		init(context);
+		this.mContext = context;
+		init(mContext);
 	}
 
 	public PathMenu(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		init(context);
+		this.mContext = context;
+		init(mContext);
 		applyAttrs(attrs);
 	}
 
@@ -49,31 +86,59 @@ public class PathMenu extends RelativeLayout {
 		LayoutInflater li = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		li.inflate(R.layout.float_menu, this);
-		
-	 
+		mWindowManager = (WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE);// 获取系统的窗口服务
+		DisplayMetrics dm = new DisplayMetrics();
+		mWindowManager.getDefaultDisplay().getMetrics(dm);
+		mScreenWidth = dm.widthPixels;// 根据当前屏幕信息拿到屏幕的宽高
+		mScreenHeight = dm.heightPixels;
+
+		this.mWmParams = new WindowManager.LayoutParams();// 获取窗口参数
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			mWmParams.type = WindowManager.LayoutParams.TYPE_TOAST;// 等于API19或API19以下需要指定窗口参数type值为TYPE_TOAST才可以作为悬浮控件显示出来
+		} else {
+			mWmParams.type = WindowManager.LayoutParams.TYPE_PHONE;// API19以上侧只需指定为TYPE_PHONE即可
+		}
+		mWmParams.format = PixelFormat.RGBA_8888;// 当前窗口的像素格式为RGBA_8888,即为最高质量
+
+		// NOT_FOCUSABLE可以是悬浮控件可以响应事件，LAYOUT_IN_SCREEN可以指定悬浮球指定在屏幕内，部分虚拟按键的手机，虚拟按键隐藏时，虚拟按键的位置则属于屏幕内，此时悬浮球会出现在原虚拟按键的位置
+		mWmParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+				| WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+		// 默认指定位置在屏幕的左上方，可以根据需要自己修改
+		mWmParams.gravity = Gravity.LEFT | Gravity.TOP;
+		position = 1;
+		// 默认指定的横坐标为屏幕一半
+		mWmParams.x = mScreenWidth / 2;
+		// 默认指定的纵坐标为屏幕高度的一半，这里只是大概约束，因为上的flags参数限制，悬浮球不会出现在屏幕外
+		mWmParams.y = mScreenHeight / 2;
+
+		// 宽度指定为内容自适应
+		mWmParams.width = LayoutParams.WRAP_CONTENT;
+		mWmParams.height = LayoutParams.WRAP_CONTENT;
 
 		mPathMenuLayout = (PathMenuLayout) findViewById(R.id.item_layout);
-
 		final ViewGroup controlLayout = (ViewGroup) findViewById(R.id.control_layout);
 		controlLayout.setClickable(true);
-		controlLayout.setOnTouchListener(new OnTouchListener() {
-			// 点击展开
+		controlLayout.setOnTouchListener(this);
 
+		btnWidth = controlLayout.getLayoutParams().width;
+		btnHeight = controlLayout.getLayoutParams().height;
+		mHintView = (ImageView) findViewById(R.id.control_hint);
+		mWindowManager.addView(this, mWmParams);
+		initPathMenu(this, ITEM_DRAWABLES);// 初始化子菜单
+
+		controlLayout.setOnClickListener(new OnClickListener() {
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				if (event.getAction() == MotionEvent.ACTION_DOWN) {
-					
+			public void onClick(View v) {
+				if (!mDraging) {
 					mHintView
 							.startAnimation(createHintSwitchAnimation(mPathMenuLayout
 									.isExpanded()));
 					mPathMenuLayout.switchState(true);
 				}
-				
-				return false;
 			}
 		});
-
-		mHintView = (ImageView) findViewById(R.id.control_hint);
 	}
 
 	/**
@@ -97,7 +162,6 @@ public class PathMenu extends RelativeLayout {
 			int newChildSize = a.getDimensionPixelSize(
 					R.styleable.ArcLayout_childSize, defaultChildSize);
 			mPathMenuLayout.setChildSize(newChildSize);
-
 			a.recycle();
 		}
 	}
@@ -237,5 +301,140 @@ public class PathMenu extends RelativeLayout {
 		animation.setFillAfter(true);
 
 		return animation;
+	}
+
+	/**
+	 * 触摸事件
+	 */
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		float x = (int) event.getRawX();
+		float y = (int) event.getRawY();
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			mTouchStartX = event.getX();
+			mTouchStartY = event.getY();
+			mWmParams.alpha = 1f;
+			mWindowManager.updateViewLayout(this, mWmParams);
+			mDraging = false;
+			break;
+
+		case MotionEvent.ACTION_MOVE:
+			float mMoveStartX = event.getX();
+			float mMoveStartY = event.getY();
+			if (Math.abs(mTouchStartX - mMoveStartX) > 3
+					&& Math.abs(mTouchStartY - mMoveStartY) > 3) {
+				mDraging = true;
+				mWmParams.x = (int) (x - mTouchStartX);
+				mWmParams.y = (int) (y - mTouchStartY);
+				mWindowManager.updateViewLayout(this, mWmParams);
+				return false;
+			}
+			break;
+
+		case MotionEvent.ACTION_UP:
+		case MotionEvent.ACTION_CANCEL:
+			int wmX = mWmParams.x;
+			int wmY = mWmParams.y;
+			if (wmX <= mScreenWidth / 4) //左边  竖区域
+			{
+				if (wmY <= mScreenHeight / 4) {
+					position = LEFT_TOP;//左上
+				} else if (wmY >= mScreenHeight / 4
+						&& wmY <= mScreenHeight * 3 / 4) {
+					position = LEFT_CENTER;//左中
+				} else if (wmY >= mScreenHeight * 3 / 4) {
+					position = LEFT_BOTTOM;//左下
+				}
+			}
+			else if(wmX>=mScreenWidth / 4 && wmX<=mScreenWidth *3/4)//中间 竖区域
+			{
+				if (wmY <= mScreenHeight / 4) {
+					position = CENTER_TOP;//中上
+				} else if (wmY >= mScreenHeight / 4
+						&& wmY <= mScreenHeight * 3 / 4) {
+					position = CENTER;//中
+				} else if (wmY >= mScreenHeight * 3 / 4) {
+					position = CENTER_BOTTOM;//中下
+				}
+			}
+			
+			else if(wmX>=mScreenWidth *3/4)//右边竖区域
+			{
+				if (wmY <= mScreenHeight / 4) {
+					position = RIGHT_TOP;//上右
+				} else if (wmY >= mScreenHeight / 4
+						&& wmY <= mScreenHeight * 3 / 4) {
+					position = RIGHT_CENTER;//中右
+				} else if (wmY >= mScreenHeight * 3 / 4) {
+					position = RIGHT_BOTTOM;//下右
+				}
+			}
+			refreshPathMenu(position);
+			mWindowManager.updateViewLayout(this, mWmParams);
+			mTouchStartX = mTouchStartY = 0;
+			break;
+		}
+		return false;
+	}
+
+	/**
+	 * 根据按钮位置改变子菜单方向
+	 * 
+	 * @param position
+	 */
+	private void refreshPathMenu(int position) {
+		if (position == LEFT_TOP) {// 上左
+			mPathMenuLayout.setArc(0, 90);
+		} else if (position == CENTER_TOP) {// 上中
+			mPathMenuLayout.setArc(0, 180);
+		} else if (position == RIGHT_TOP) {// 上右
+			mPathMenuLayout.setArc(90, 180);
+		} else if (position == LEFT_CENTER) {// 中左
+			mPathMenuLayout.setArc(270, 270 + 180);
+		} else if (position == CENTER) {// 中
+			mPathMenuLayout.setArc(0, 360);
+		} else if (position == RIGHT_CENTER) {// 中右
+			mPathMenuLayout.setArc(90, 270);
+		} else if (position == LEFT_BOTTOM) { // 下左
+			mPathMenuLayout.setArc(270, 360);
+		} else if (position == CENTER_BOTTOM) {// 下中
+			mPathMenuLayout.setArc(180, 360);
+		} else if (position == RIGHT_BOTTOM) {// 下右
+			mPathMenuLayout.setArc(180, 270);
+		}
+
+	}
+
+	/**
+	 * 初始化子菜单图片、点击事件
+	 * 
+	 * @param menu
+	 * @param itemDrawables
+	 */
+	private void initPathMenu(PathMenu menu, int[] itemDrawables) {
+		final int itemCount = itemDrawables.length;
+		for (int i = 0; i < itemCount; i++) {
+			ImageView item = new ImageView(mContext);
+			item.setImageResource(itemDrawables[i]);
+			final int index = i;
+			menu.addItem(item, new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					switch (index) {
+					case 0:
+						Toast.makeText(getContext(), "第0个被点击",
+								Toast.LENGTH_SHORT).show();
+						break;
+
+					case 1:
+						Toast.makeText(getContext(), "第1个被点击",
+								Toast.LENGTH_SHORT).show();
+						break;
+					}
+				}
+			});
+		}
 	}
 }
